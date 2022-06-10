2302b45809bc0770ca9bf0e8356435a188db00ca
SebasDany <sguandinangod@est.ups.edu.ec>
2022-06-09 23:33:10 -0500

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { PrintService } from '../services/print.service';
+
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,31 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(private print:PrintService) {}
+  bluetoothList:any=[];
+  selectedPrinter:any;
+  //This will list all of your bluetooth devices
+  listPrinter() { 
+    this.print.searchBluetoothPrinter()
+     .then(resp=>{
+ 
+      //List of bluetooth device list
+      this.bluetoothList=resp;
+  });
+}
+//This will store selected bluetooth device mac address
+selectPrinter(macAddress)
+{
+//Selected printer macAddress stored here
+this.selectedPrinter=macAddress;
+}
+
+//This will print
+printStuff()
+{  
+ //The text that you want to print
+ var myText="Hello hello hello \n\n\n This is a test \n\n\n";
+ this.print.sendToBluetoothPrinter(this.selectedPrinter,myText);
+}
 
 }
